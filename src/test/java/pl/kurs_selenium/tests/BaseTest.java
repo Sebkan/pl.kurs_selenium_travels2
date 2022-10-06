ddca612eb791a9f2848035eda3458a7ff185781f
Sebastian Kanecki <seb.kanecki@gmail.com>
2022-10-06 14:37:48 +0200

    Added Extent reports in BaseTest methods; Added get Screenshot method combined with reports.

--- src/test/java/pl/kurs_selenium/tests/BaseTest.java
+++ src/test/java/pl/kurs_selenium/tests/BaseTest.java
@@ -1,13 +1,31 @@
 package pl.kurs_selenium.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import pl.kurs_selenium.utils.DriverFactory;
 
 public class BaseTest {
-    protected static WebDriver driver;
+    public static ExtentHtmlReporter extentHtmlReporter;
+    public static ExtentReports extentReports;
 
+    protected WebDriver driver;
+
+    @BeforeSuite
+    public static void beforeSuite(){
+        extentHtmlReporter = new ExtentHtmlReporter("index.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(extentHtmlReporter);
+    }
+    @AfterSuite
+    public static void afterSuite() {
+        extentHtmlReporter.flush();
+        extentReports.flush();
+    }
     @BeforeMethod
     public  void setup() {
         driver = DriverFactory.getDriver("chrome");
